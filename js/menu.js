d5164b97e6ad9edcf2796dc7392dc094c66a758f
MishaGl <mishaglansev@gmail.com>
2017-10-28 18:08:54 +0300

    formprefinal

--- js/menu.js
+++ js/menu.js
@@ -3,10 +3,15 @@
   var navToggle = document.querySelector('.header__button-toggle');
   var header = document.querySelector('.header__wrapper--nojs');
   var pagetitle = document.querySelector('.page-title__block--nojs');
+  var pagephoto = document.querySelector('.page-photo__intro--nojs');
+  var pageform = document.querySelector('.form-page__intro--nojs');
+
 
   navMain.classList.remove('navigation--nojs');
   header.classList.remove('header__wrapper--nojs');
   pagetitle.classList.remove('page-title__block--nojs');
+  pagephoto.classList.remove('page-photo__intro--nojs');
+  pageform.classList.remove('form-page__intro--nojs');
 
   navToggle.addEventListener('click', function() {
     if (navMain.classList.contains('navigation--closed')) {
